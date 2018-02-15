a1b0dfcb6d76ea802ed41122e104e1aa714fd44b
katherineRamirez <oeillet8_5@hotmail.com>
2018-02-15 16:30:36 -0300

    primer commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,23 @@
-fetch('http://gateway.marvel.com/v1/public/characters?apikey=fd35aba799605ca550a4c2f9666a6f50&ts=9&hash=62865700f7c07e460304b6c2ceb6bf1a')
+fetch('http://gateway.marvel.com/v1/public/comics?apikey=fd35aba799605ca550a4c2f9666a6f50&ts=9&hash=62865700f7c07e460304b6c2ceb6bf1a')
   .then(function (response) {
     //Turns the the JSON into a JS object
     return response.json();
   })
   .then(function (data) {
     console.log(data);
+    
+   for(let i = 0; i < data.data.results.length; i++){
+   	//console.log(i);
+   	//console.log(data.data.results[i].comics);
+   	console.log(data.data.results[0].thumbnail);
+   	let path = data.data.results[0].thumbnail.path;
+   	//console.log(data.data.results[0].thumbnail.path);
+   	let extension = data.data.results[0].thumbnail.extension;
+   	console.log(path +'.' +extension);
+   	
+   }
+
+  
 
     //Sección donde aparecerán los resultados
   });
