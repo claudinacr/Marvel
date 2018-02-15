b50108d9c7411466505fa8bb53c5bcfe4904c0fd
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-15 16:32:24 -0300

    Segundo commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,14 @@
-fetch('http://gateway.marvel.com/v1/public/characters?apikey=fd35aba799605ca550a4c2f9666a6f50&ts=9&hash=62865700f7c07e460304b6c2ceb6bf1a')
+fetch('http://gateway.marvel.com/v1/public/comics?apikey=fd35aba799605ca550a4c2f9666a6f50&ts=9&hash=62865700f7c07e460304b6c2ceb6bf1a')
   .then(function (response) {
     //Turns the the JSON into a JS object
     return response.json();
   })
   .then(function (data) {
     console.log(data);
+    for (let i = 0; i < data.data.results.length; i++) {
+
+    }
+
 
     //Sección donde aparecerán los resultados
   });
