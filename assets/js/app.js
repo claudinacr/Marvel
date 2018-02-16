4f4fdea95ff34733623ba965a3507c47381aa4e7
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-16 09:13:52 -0300

    Implementación de modales

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,25 +11,48 @@ fetch('http://gateway.marvel.com/v1/public/comics?apikey=fd35aba799605ca550a4c2f
     }
     let ul = document.createElement('ul');
     let imagen = document.querySelector('.imagen');
-
     for (let i = 0; i < data.data.results.length; i++) {
       let title = data.data.results[i].title;
       if (title === array[i]) {
         let box = document.createElement('div');
         box.className = 'box';
-        let boxImage = document.createElement('div');
+        box.setAttribute('data-toggle', 'modal');
+        box.setAttribute('data-target', '#miModal');
+        //CONTENIDO DEL MODAL
+        // TITULO DEL COMICS
+        // IMAGEN DEL COMICS
+
         let image = document.createElement('img');
         image.setAttribute('src', marvel[i].image);
+        let boxImage = document.createElement('a');
         boxImage.appendChild(image);
         let boxText = document.createElement('div');
         let text = document.createTextNode(title);
-        text.className='text';
+        text.className = 'text';
         let p = document.createElement('p');
         p.appendChild(text);
         boxText.appendChild(p);
         box.appendChild(boxImage);
-        box.appendChild(boxText);  
-        imagen.appendChild(box);      
+        box.appendChild(boxText);
+        imagen.appendChild(box);
+        box.addEventListener('click', function (event) {
+          let objetivo = event.target.getAttribute('src');
+          console.log(objetivo);
+         for (let k = 0; k < marvel.length; k++) {
+           if (objetivo === marvel[k].image) {
+             let textInfo = marvel.text;
+             let myModalLabel = document.querySelector('#myModalLabel');
+             myModalLabel.innerHTML='';
+             let titlee = data.data.results[k].title;
+             myModalLabel.appendChild(document.createTextNode(titlee));
+           }
+         }
+        });
+        
       }
+      
     }
+
+    
   });
+
