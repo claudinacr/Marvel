d306e49add977b28c7e9f9495c43e01f06ef4ed4
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-16 11:47:15 -0300

    reload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,6 @@
+
+
+
 fetch('http://gateway.marvel.com/v1/public/comics?apikey=fd35aba799605ca550a4c2f9666a6f50&ts=9&hash=62865700f7c07e460304b6c2ceb6bf1a')
   .then(function (response) {
     //Turns the the JSON into a JS object
@@ -75,9 +78,12 @@ fetch('http://gateway.marvel.com/v1/public/comics?apikey=fd35aba799605ca550a4c2f
 
         } else{
           boxx[marvel].style.display='none';
-
         }
         
+      }
+
+      if (valor === '') {
+        location.reload();
       }
 
 
