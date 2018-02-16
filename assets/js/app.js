9a7f54f98ed031c232991b6fe7c84f45b797ed8e
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-16 07:20:06 -0300

    Mejora de estilos y código

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,24 +4,32 @@ fetch('http://gateway.marvel.com/v1/public/comics?apikey=fd35aba799605ca550a4c2f
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    console.log(data.data.results);
+    let array = [];
+    for (let j = 0; j < marvel.length; j++) {
+      array.push(marvel[j].title);
+    }
+    let ul = document.createElement('ul');
+    let imagen = document.querySelector('.imagen');
 
     for (let i = 0; i < data.data.results.length; i++) {
-      //console.log(i);
-      //console.log(data.data.results[i].comics);
-      // let path = data.data.results[0].thumbnail.path;
-      // //console.log(data.data.results[0].thumbnail.path);
-      // let extension = data.data.results[0].thumbnail.extension;
-      // console.log(path + '.' + extension);
-      // let image = document.createElement('img');
-      // image.className = 'imagen';
-      // let imagen = path + '.' + extension;
-      // image.setAttribute('src', imagen);
-      // let imagenes = document.querySelector('.imagen');
-      // imagenes.appendChild(image);
       let title = data.data.results[i].title;
-      console.log(title);
-
+      if (title === array[i]) {
+        let box = document.createElement('div');
+        box.className = 'box';
+        let boxImage = document.createElement('div');
+        let image = document.createElement('img');
+        image.setAttribute('src', marvel[i].image);
+        boxImage.appendChild(image);
+        let boxText = document.createElement('div');
+        let text = document.createTextNode(title);
+        text.className='text';
+        let p = document.createElement('p');
+        p.appendChild(text);
+        boxText.appendChild(p);
+        box.appendChild(boxImage);
+        box.appendChild(boxText);  
+        imagen.appendChild(box);      
+      }
     }
-
   });
