bfaf20594f22d1b51e09005e05c3e78a074d256a
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-17 07:55:56 -0300

    Actualización de imagenes

--- assets/js/marvel.js
+++ assets/js/marvel.js
@@ -1,81 +1,81 @@
 var marvel = [{
     "title": "The Amazing Spider-Man (2017) #22 (Lozano Teaser Variant)",
-    "image": "assets/images/images marvel/CapTransport.jpg",
+    "image": "assets/images/images marvel/The Amazing Spider-Man (2017).jpg",
   },
   {
     "title": "Cap Transport (2005) #4",
-    "image": "assets/images/images marvel/ant_man.jpg",
+    "image": "assets/images/images marvel/I-am-Cap-Astonishing-X-Men-40.jpg",
   },
   {
     "title": "Ant-Man (2003) #3",
-    "image": "assets/images/images marvel/X-Men_Forever_1_cover.jpg",
+    "image": "assets/images/images marvel/90ac0f437dfd53520b00e5f88bf3f7d0.jpg",
   },
   {
     "title": "Hulk Custom Comic (2008) #1",
-    "image": "assets/images/images marvel/Ultimate_Spider-Man_boxart.jpg",
+    "image": "assets/images/images marvel/33bd5af72a5947e952f34078963591f2.jpg",
   },
   {
     "title": "Ultimate X-Men (Spanish Language Edition) (2000)",
-    "image": "assets/images/images marvel/61XYk9+3XQL._SX317_BO1,204,203,200_.jpg",
+    "image": "assets/images/images marvel/e1e480e06d02cdef28c24a98ae494197.jpg",
   },
   {
     "title": "Ultimate Spider-Man (Spanish Language Edition) (2000)",
-    "image": "assets/images/images marvel/602409.jpg",
+    "image": "assets/images/images marvel/108d85053433be2644fe05a897a48bdc.jpg",
   },
   {
     "title": "Marvel Adventures Super Heroes Special (2010) #1",
-    "image": "assets/images/images marvel/cbc375f993a4235468cc806bc1bcad42_l.jpg",
+    "image": "assets/images/images marvel/5acab9ebd53ba606f9d3341db51062bd.jpg",
   },
   {
     "title": "Black Panther (2016) #4 (Anacleto Mighty Men Variant",
-    "image": "assets/images/images marvel/twim_post_master-1.jpg",
+    "image": "assets/images/images marvel/624609a031b81cff27e0de3306e6abb1.jpg",
   },
   {
     "title": "Marvels Vol. 1 (1994) #7",
-    "image": "assets/images/images marvel/descarga.jpeg",
+    "image": "assets/images/images marvel/a04a086f8509782b9cd9a2b3135f13af.jpg",
   },
   {
     "title": "Kabuki Reflections Vol. 1 (Hardcover)",
-    "image": "assets/images/images marvel/descarga (1).jpeg",
+    "image": "assets/images/images marvel/e641bfa4913e8ae93334ad54d100a97d.jpg",
   },
   {
     "title": "Doctor Strange (2015) #10 (Henderson Mighty Men Variant)",
-    "image": "assets/images/images marvel/250px-X-Men_208.jpg",
+    "image": "assets/images/images marvel/6161dc857c90c26e63c860d6c0731192.jpg",
   },
   {
     "title": "Cap Transport (2005) #16",
-    "image": "assets/images/images marvel/descarga (2).jpeg",
+    "image": "assets/images/images marvel/I-am-Cap-Astonishing-X-Men-40.jpg",
   },
   {
     "title": "Ultimate X-Men (Spanish Language Edition) (2000) #6",
-    "image": "assets/images/images marvel/portrait_incredible.jpg",
+    "image": "assets/images/images marvel/813dac3e09d69018303a42eb1f80e8f0.jpg",
   },
   {
     "title": "Holiday Special (1969) #1",
-    "image": "assets/images/images marvel/pportrait_incredible (1).jpg",
+    "image": "assets/images/images marvel/5346b34b12c58a368d07f6b944c1786a.jpg",
   },
   {
     "title": "Ultimate Spider-Man (Spanish Language Edition) (2000) #7",
-    "image": "assets/images/images marvel/FEB062957._SX314_QL80_TTD_.jpg",
+    "image": "assets/images/images marvel/4dbd88107b912fd796f58c3a6f7bb06e.jpg",
   },
   {
     "title": "Civil War II (2016) #6 (Sprouse Battle Variant)",
-    "image": "assets/images/images marvel/X-Factor_Annual_Vol_1_9.jpg",
+    "image": "assets/images/images marvel/3588094efa3b5883dfb51c93c859f353.jpg",
   },
   {
     "title": "Cap Transport (2005) #1",
-    "image": "assets/images/images marvel/descarga (3).jpeg",
+    "image": "assets/images/images marvel/9eec90a482dedb8be183bbe024c8e4b9.jpg",
   },
   {
     "title": "Cap Transport (2005) #6",
-    "image": "assets/images/images marvel/portrait_incredible (1).jpg",
+    "image": "assets/images/images marvel/031060bbc43f3f83041f90f9b9dc50d5.jpg",
   },
   {
     "title": "Ultimate X-Men (Spanish Language Edition) (2000) #12",
-    "image": "assets/images/images marvel/IronFist-Master.jpg",
+    "image": "assets/images/images marvel/90b061906201c8571e16b51ebb3f5a56.jpg",
   },
   {
     "title": "Ultimate Spider-Man (Spanish Language Edition) (2000) #11",
-    "image": "assets/images/images marvel/unnamed.jpg",
+    "image": "assets/images/images marvel/fdad3540a00fa138978f7bc1b2a43873.jpg",
   }
 ];
