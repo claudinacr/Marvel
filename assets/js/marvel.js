c38610f962078e000d663c692e7f1e36808d26a8
Claudina Cortez <claudinacr@gmail.com>
2018-03-13 15:26:28 -0300

    Actualización de títulos

--- assets/js/marvel.js
+++ assets/js/marvel.js
@@ -1,81 +1,81 @@
 var marvel = [{
-    "title": "Cap Transport (2005) #18)",
+    "title": "Ultimate X-Men (Spanish Language Edition) (2000) #8",
     "image": "assets/images/images marvel/The Amazing Spider-Man (2017).jpg",
   },
   {
-    "title": "Gun Theory (2003) #4",
+    "title": "Magician: Apprentice Riftwar Saga (2010) #16",
     "image": "assets/images/images marvel/I-am-Cap-Astonishing-X-Men-40.jpg",
   },
   {
-    "title": "Ultimate X-Men (Spanish Language Edition) (2000) #8",
+    "title": "The Amazing Spider-Man (2017) #19 (Veregge Black Panther 50th Anniversary Variant)",
     "image": "assets/images/images marvel/90ac0f437dfd53520b00e5f88bf3f7d0.jpg",
   },
   {
-    "title": "Squadron Supreme (2015) #9 (Frison Mighty Men Variant)",
+    "title": "Hedge Knight II: Sworn Sword (2007) #1 (Yu Variant)",
     "image": "assets/images/images marvel/33bd5af72a5947e952f34078963591f2.jpg",
   },
   {
-    "title": "Marvels Vol. 1 (1994) #1",
+    "title": "Cap Transport (2005) #19",
     "image": "assets/images/images marvel/e1e480e06d02cdef28c24a98ae494197.jpg",
   },
   {
-    "title": "Universe X: Cap (2001)",
+    "title": "Ultimate X-Men (Spanish Language Edition) (2000) #10",
     "image": "assets/images/images marvel/108d85053433be2644fe05a897a48bdc.jpg",
   },
   {
-    "title": "Amazing Spider-Man (1999) #558 (Turner Variant)",
+    "title": "Ultimate Spider-Man (Spanish Language Edition) (2000) #3",
     "image": "assets/images/images marvel/5acab9ebd53ba606f9d3341db51062bd.jpg",
   },
   {
-    "title": "The Astonishing Ant-Man (2015) #10 (Bagley Mighty Men Variant)",
+    "title": "Punishermax: Kingpin (2010)",
     "image": "assets/images/images marvel/624609a031b81cff27e0de3306e6abb1.jpg",
   },
   {
-    "title": "Cap Transport (2005) #8",
+    "title": "Marvels Vol. 1 (1994) #6",
     "image": "assets/images/images marvel/a04a086f8509782b9cd9a2b3135f13af.jpg",
   },
   {
-    "title": "Halo Chronicles (2009) #2",
+    "title": "Incredible Hulks (2009) #604 (DJURDJEVIC 70TH ANNIVERSARY VARIANT)",
     "image": "assets/images/images marvel/e641bfa4913e8ae93334ad54d100a97d.jpg",
   },
   {
-    "title": "Ultimate X-Men (Spanish Language Edition) (2000) #10",
+    "title": "Amazing Spider-Man (1999) #558 (Turner Variant)",
     "image": "assets/images/images marvel/6161dc857c90c26e63c860d6c0731192.jpg",
   },
   {
-    "title": "Ultimate Spider-Man (Spanish Language Edition) (2000) #3",
+    "title": "Silver Surfer (1987)",
     "image": "assets/images/images marvel/I-am-Cap-Astonishing-X-Men-40.jpg",
   },
   {
-    "title": "Ultimate Spider-Man Ultimate Collection Book 1 (Trade Paperback)",
+    "title": "Cap Transport (2005) #12",
     "image": "assets/images/images marvel/813dac3e09d69018303a42eb1f80e8f0.jpg",
   },
   {
-    "title": "Magician: Apprentice Riftwar Saga (2010) #15",
+    "title": "All-New, All-Different Avengers (2015) #12 (Kuder Mighty Men Variant)",
     "image": "assets/images/images marvel/5346b34b12c58a368d07f6b944c1786a.jpg",
   },
   {
-    "title": "Official Marvel Universe Handbook (2009) #2",
+    "title": "Cap Transport (2005) #9",
     "image": "assets/images/images marvel/4dbd88107b912fd796f58c3a6f7bb06e.jpg",
   },
   {
-    "title": "Cap Transport (2005) #11",
+    "title": "The Totally Awesome Hulk (2015) #8 (Putri Mighty Men Variant)",
     "image": "assets/images/images marvel/3588094efa3b5883dfb51c93c859f353.jpg",
   },
   {
-    "title": "Wolverine Saga (2009) #7",
+    "title": "Civil War II (2016) #6 (Gi Connecting Variant H)",
     "image": "assets/images/images marvel/9eec90a482dedb8be183bbe024c8e4b9.jpg",
   },
   {
-    "title": "Cap Transport (2005) #2",
+    "title": "The Amazing Spider-Man (2017) #15 (Panosian Mighty Men Variant)",
     "image": "assets/images/images marvel/031060bbc43f3f83041f90f9b9dc50d5.jpg",
   },
   {
-    "title": "Gun Theory (2003) #3",
+    "title": "Invincible Iron Man (2015) #11 (Keown Mighty Men Variant)",
     "image": "assets/images/images marvel/90b061906201c8571e16b51ebb3f5a56.jpg",
   },
   {
-    "title": "Marvels Vol. 1 (1994) #4",
+    "title": "Spider-Man: Big Time (2010) #12",
     "image": "assets/images/images marvel/fdad3540a00fa138978f7bc1b2a43873.jpg",
   }
 ];
